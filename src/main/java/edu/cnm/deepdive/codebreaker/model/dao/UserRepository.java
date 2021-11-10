abf36c0eca03c8ceed39444d2999fdfa9b3c8d9c
nmitchell313 <nmitchell11@cnm.edu>
2021-11-10 10:02:21 -0700

    finsished user service. game service (in progress).

--- src/main/java/edu/cnm/deepdive/codebreaker/model/dao/UserRepository.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/dao/UserRepository.java
@@ -4,10 +4,14 @@ import edu.cnm.deepdive.codebreaker.model.entity.User;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
   Optional<User> findByOauthKey(String oauthKey);
 
   Optional<User> findByExternalKey(UUID externalKey);
+
+  Iterable<User> getAllByOrderByDisplayNameAsc();
+
 }
